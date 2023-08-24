94dc71bb10bc4b36aec913a840f2a515abaa9b6d
Pulkit077 <pulkitagarwal2k1@gmail.com>
2023-08-24 15:23:11 +0530

    drivers: power: supply: qcom: import power changes from sweet_k6a
     - from sweet_k6a-r-oss kernel source
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- drivers/power/supply/ti/bq2597x_charger.c
+++ drivers/power/supply/ti/bq2597x_charger.c
@@ -1699,7 +1699,7 @@ static int bq2597x_init_protection(struct bq2597x *bq)
 	ret = bq2597x_set_acovp_th(bq, bq->cfg->ac_ovp_th);
 	bq_info("set ac ovp threshold %d %s\n", bq->cfg->ac_ovp_th,
 		!ret ? "successfully" : "failed");
-
+	bq2597x_dump_reg(bq);
 	return 0;
 }
 
@@ -2033,17 +2033,7 @@ static int bq2597x_charger_get_property(struct power_supply *psy,
 		val->intval = 0;
 		break;
 	case POWER_SUPPLY_PROP_MODEL_NAME:
-		ret = bq2597x_get_work_mode(bq, &bq->mode);
-		if (ret) {
-			val->strval = "unknown";
-		} else {
-			if (bq->mode == BQ25970_ROLE_MASTER)
-				val->strval = "bq2597x-master";
-			else if (bq->mode == BQ25970_ROLE_SLAVE)
-				val->strval = "bq2597x-slave";
-			else
-				val->strval = "bq2597x-standalone";
-		}
+		val->strval = "bq2597x-master";
 		break;
 	case POWER_SUPPLY_PROP_TI_BUS_ERROR_STATUS:
 		val->intval = bq2597x_check_vbus_error_status(bq);
@@ -2056,6 +2046,7 @@ static int bq2597x_charger_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CP_VBAT_CALIBRATE:
 		val->intval = bq->vbat_calibrate;
+		break;
 	default:
 		return -EINVAL;
 
@@ -2120,14 +2111,7 @@ static int bq2597x_psy_register(struct bq2597x *bq)
 {
 	bq->psy_cfg.drv_data = bq;
 	bq->psy_cfg.of_node = bq->dev->of_node;
-
-	if (bq->mode == BQ25970_ROLE_MASTER)
-		bq->psy_desc.name = "bq2597x-master";
-	else if (bq->mode == BQ25970_ROLE_SLAVE)
-		bq->psy_desc.name = "bq2597x-slave";
-	else
-		bq->psy_desc.name = "bq2597x-standalone";
-
+	bq->psy_desc.name = "bq2597x-master";
 	bq->psy_desc.type = POWER_SUPPLY_TYPE_MAINS;
 	bq->psy_desc.properties = bq2597x_charger_props;
 	bq->psy_desc.num_properties = ARRAY_SIZE(bq2597x_charger_props);
@@ -2352,12 +2336,7 @@ static const struct file_operations reg_debugfs_ops = {
 
 static void create_debugfs_entry(struct bq2597x *bq)
 {
-	if (bq->mode == BQ25970_ROLE_MASTER)
-		bq->debug_root = debugfs_create_dir("bq2597x-master", NULL);
-	else if (bq->mode == BQ25970_ROLE_SLAVE)
-		bq->debug_root = debugfs_create_dir("bq2597x-slave", NULL);
-	else
-		bq->debug_root = debugfs_create_dir("bq2597x-standalone", NULL);
+	bq->debug_root = debugfs_create_dir("bq2597x-master", NULL);
 
 	if (!bq->debug_root)
 		bq_err("Failed to create debug dir\n");
@@ -2380,17 +2359,8 @@ static void create_debugfs_entry(struct bq2597x *bq)
 
 static struct of_device_id bq2597x_charger_match_table[] = {
 	{
-		.compatible = "ti,bq2597x-standalone",
-		.data = &bq2597x_mode_data[BQ25970_STDALONE],
-	},
-	{
 		.compatible = "ti,bq2597x-master",
-		.data = &bq2597x_mode_data[BQ25970_MASTER],
-	},
-
-	{
-		.compatible = "ti,bq2597x-slave",
-		.data = &bq2597x_mode_data[BQ25970_SLAVE],
+		.data = &bq2597x_mode_data[BQ25970_STDALONE],
 	},
 	{},
 };
@@ -2554,7 +2524,6 @@ static int bq2597x_resume(struct device *dev)
 	}
 
 	bq2597x_enable_adc(bq, true);
-	power_supply_changed(bq->fc2_psy);
 	bq_err("Resume successfully!");
 
 	return 0;
